fc6457b90cebdda2ee8feb5514317544b271a5cc
Johannes Löthberg <johannes@kyriasis.com>
2015-01-24 00:44:41 +0100

    docs: Add info and index for html docs

--- docs/conf.py
+++ docs/conf.py
@@ -2,16 +2,26 @@
 # -*- coding: utf-8 -*-
 
 project = 'YASL'
-master_doc = 'yasl'
+copyright = '2014-2015, The yasl developers'
+master_doc = 'index'
 source_suffix = '.rst'
 
 show_authors = False
 today_fmt = '%d %b %Y'
 
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+exclude_patterns = ['_build']
+
+highlight_language = 'c'
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = 'yasldoc'
+
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('yasl', 'yasl', 'Yet Another String Library for C', [], 3),
-    ('yaslnew', 'yaslnew', 'Create a new yasl string', [], 3),
-    ('yaslfree', 'yaslfree', 'Free a yasl string', [], 3),
+    ('yasl',     'yasl',     'Yet Another String Library for C', [], 3),
+    ('yaslnew',  'yaslnew',  'Create a new yasl string',         [], 3),
+    ('yaslfree', 'yaslfree', 'Free a yasl string',               [], 3),
 ]
---
+++ docs/index.rst
@@ -0,0 +1,19 @@
+====================
+ Welcome to yasldoc
+====================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   API
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`search`
+
