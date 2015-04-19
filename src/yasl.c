3379ff41ef93f52f5bf2a9d25fdeb0738f3d5eb0
Johannes Löthberg <johannes@kyriasis.com>
2015-04-19 19:01:38 +0200

    Replace hex_digit_to_int with ternary return version

--- src/yasl.c
+++ src/yasl.c
@@ -690,24 +690,9 @@ yaslRemoveFreeSpace(yastr str) {
  * integer from 0 to 15 */
 int
 hex_digit_to_int(char c) {
-	switch(c) {
-	case '0': return 0;
-	case '1': return 1;
-	case '2': return 2;
-	case '3': return 3;
-	case '4': return 4;
-	case '5': return 5;
-	case '6': return 6;
-	case '7': return 7;
-	case '8': return 8;
-	case '9': return 9;
-	case 'a': case 'A': return 10;
-	case 'b': case 'B': return 11;
-	case 'c': case 'C': return 12;
-	case 'd': case 'D': return 13;
-	case 'e': case 'E': return 14;
-	case 'f': case 'F': return 15;
-	default: return 0;
-	}
+	// Eg. 'B' - 'A' is 0, so we need to add 10 to get the correct value.
+	return c >= '0' && c <= '9' ? c - '0'      :
+	       c >= 'A' && c <= 'F' ? c - 'A' + 10 :
+	       c >= 'a' && c <= 'f' ? c - 'a' + 10 : 0 ;
 }
 
